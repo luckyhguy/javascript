5a5744528f7d7bb178b95b93df9a7628940a2087
H <H@Rocca.local>
2017-03-06 16:17:25 +0900

    .

---
+++ lecture04.js
@@ -0,0 +1,15 @@
+//자료형의 종류
+
+var a=100, b=3.14; // number type (숫자)
+
+var c="안녕하세요", d="a"; // string type(문자열)
+
+var e=true, f=false; // boolean type (맞다 / 틀리다)
+
+
+console.log(a, typeof(a));
+// 100, "number" 라고 출력
+console.log(c, typeof(c));
+// 안녕하세요, "string" 라고 출력
+console.log(e, typeof(e));
+// true, "boolean" 라고 출력
